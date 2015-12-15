b69ef1e86ddc4c591418688d072640fd8faed79f
senth <senth.wallace@gmail.com>
2015-12-15 11:50:29 +0100

    Initial project

---
+++ src/main/java/se/lu/sysa11/schinnbehn/Application.java
@@ -0,0 +1,14 @@
+package se.lu.sysa11.schinnbehn;
+
+/**
+ * @author Matteus Magnusson <matteus.magnusson@spiddekauga.com>
+ */
+public class Application {
+	/**
+	 * Main class
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+
+	}
+}

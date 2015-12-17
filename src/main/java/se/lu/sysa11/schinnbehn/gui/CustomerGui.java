7d215884ba531b204590b58c294a572e9b787f3a
paulssonkalle <paulsson.kalle@gmail.com>
2015-12-17 15:30:24 +0100

    började på customergui

--- src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
+++ src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
@@ -1,21 +1,153 @@
 package se.lu.sysa11.schinnbehn.gui;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
 /**
  * @author senth
  */
 public class CustomerGui extends Gui {
+	private JPanel panel = new JPanel();
+	private JTextField textField_Name;
+	private JTextField textField_Phone;
+	private JTextField textField_Adress;
+	private JTextField textField_Email;
+	private JTextField textField_BillingAdress;
+	private JTextField textField_ContactPerson;
+	private JTextField textField_Postadress;
+	private JTextField textField_FindCustomer;
+	private JTextField textField_FoundCustomer;
+	private JTable table;
 
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	@Override
 	protected void initialize() {
+		panel.setLayout(null);
 
-		JLabel lblTest = new JLabel("Test");
-		panel.add(lblTest);
-		// TODO Auto-generated method stub
+		JLabel lblNewCustomer = new JLabel("Ny Kund");
+		lblNewCustomer.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewCustomer.setBounds(12, 28, 72, 22);
+		panel.add(lblNewCustomer);
+
+		JLabel lblName = new JLabel("Namn: ");
+		lblName.setBounds(12, 57, 56, 22);
+		panel.add(lblName);
+
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(12, 173, 56, 22);
+		panel.add(lblEmail);
+
+		JLabel lblGatuadress = new JLabel("Gatuadress:");
+		lblGatuadress.setBounds(12, 115, 72, 22);
+		panel.add(lblGatuadress);
+
+		JLabel lblPostadress = new JLabel("Postadress:");
+		lblPostadress.setBounds(12, 144, 72, 22);
+		panel.add(lblPostadress);
+
+		JLabel lblPhoneNumber = new JLabel("Telefonnr:");
+		lblPhoneNumber.setBounds(12, 86, 72, 22);
+		panel.add(lblPhoneNumber);
+
+		JLabel lblBillingAdress = new JLabel("Faktureringssadress:");
+		lblBillingAdress.setBounds(12, 202, 129, 22);
+		panel.add(lblBillingAdress);
+
+		JLabel lblContactPerson = new JLabel("Kontaktperson:");
+		lblContactPerson.setBounds(12, 231, 86, 22);
+		panel.add(lblContactPerson);
+
+		textField_Name = new JTextField();
+		textField_Name.setBounds(158, 57, 138, 22);
+		panel.add(textField_Name);
+		textField_Name.setColumns(10);
+
+		textField_Phone = new JTextField();
+		textField_Phone.setBounds(158, 86, 138, 22);
+		panel.add(textField_Phone);
+		textField_Phone.setColumns(10);
+
+		textField_Adress = new JTextField();
+		textField_Adress.setBounds(158, 115, 138, 22);
+		panel.add(textField_Adress);
+		textField_Adress.setColumns(10);
+
+		textField_Email = new JTextField();
+		textField_Email.setBounds(158, 173, 138, 22);
+		panel.add(textField_Email);
+		textField_Email.setColumns(10);
+
+		textField_BillingAdress = new JTextField();
+		textField_BillingAdress.setBounds(158, 202, 138, 22);
+		panel.add(textField_BillingAdress);
+		textField_BillingAdress.setColumns(10);
+
+		textField_ContactPerson = new JTextField();
+		textField_ContactPerson.setBounds(158, 231, 138, 22);
+		panel.add(textField_ContactPerson);
+		textField_ContactPerson.setColumns(10);
+
+		textField_Postadress = new JTextField();
+		textField_Postadress.setBounds(158, 144, 138, 22);
+		panel.add(textField_Postadress);
+		textField_Postadress.setColumns(10);
+
+		JButton btnAddCustomer = new JButton("L\u00E4gg till kund");
+		btnAddCustomer.setBounds(12, 292, 138, 25);
+		panel.add(btnAddCustomer);
+
+		JLabel lblFindCustomer = new JLabel("Hitta kund");
+		lblFindCustomer.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblFindCustomer.setBounds(12, 330, 86, 22);
+		panel.add(lblFindCustomer);
+
+		JLabel lblKundnr = new JLabel("Kundnr:");
+		lblKundnr.setBounds(12, 359, 56, 22);
+		panel.add(lblKundnr);
+
+		textField_FindCustomer = new JTextField();
+		textField_FindCustomer.setBounds(158, 359, 138, 22);
+		panel.add(textField_FindCustomer);
+		textField_FindCustomer.setColumns(10);
+
+		JButton btnSearchCustomer = new JButton("S\u00F6k");
+		btnSearchCustomer.setBounds(308, 358, 97, 25);
+		panel.add(btnSearchCustomer);
+
+		JButton btnUpdateCustomer = new JButton("Uppdatera kund");
+		btnUpdateCustomer.setBounds(158, 292, 138, 25);
+		panel.add(btnUpdateCustomer);
+
+		textField_FoundCustomer = new JTextField();
+		textField_FoundCustomer.setEditable(false);
+		textField_FoundCustomer.setBounds(158, 260, 138, 22);
+		panel.add(textField_FoundCustomer);
+		textField_FoundCustomer.setColumns(10);
+
+		JLabel lblFoundCustomer = new JLabel("Kundnr:");
+		lblFoundCustomer.setBounds(12, 260, 56, 22);
+		panel.add(lblFoundCustomer);
+
+		table = new JTable();
+		DefaultTableModel tableModel = new DefaultTableModel(new Object[][] { { "Ordernummer", "Datum", "Summa" }, },
+				new String[] { "New column", "New column", "New column" });
+		table.setModel(tableModel);
+
+		tableModel.addRow(new Object[] { "1", "2015-12-14", "333:-" });
+
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.setBounds(475, 61, 466, 319);
+		panel.add(table);
 
 	}
 }

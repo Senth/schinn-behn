998511d2cb3c2dd5e102c37afb6d54dd561d7929
senth <senth.wallace@gmail.com>
2015-12-18 10:53:03 +0100

    Added empty order and product Gui classes

---
+++ src/main/java/se/lu/sysa11/schinnbehn/gui/OrderGui.java
@@ -0,0 +1,17 @@
+package se.lu.sysa11.schinnbehn.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * @author senth
+ */
+public class OrderGui extends Gui {
+	private JPanel panel = new JPanel();
+
+	@Override
+	protected void initialize() {
+
+
+	}
+
+}
---
+++ src/main/java/se/lu/sysa11/schinnbehn/gui/ProductGui.java
@@ -0,0 +1,14 @@
+package se.lu.sysa11.schinnbehn.gui;
+
+/**
+ * @author senth
+ */
+public class ProductGui extends Gui {
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

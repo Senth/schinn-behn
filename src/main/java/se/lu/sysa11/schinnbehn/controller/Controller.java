89814d60a9a236a77ea8865983857be6e22fd03e
paulssonkalle <paulsson.kalle@gmail.com>
2015-12-18 14:01:57 +0100

    Kopplat Customer_Gui till controller

--- src/main/java/se/lu/sysa11/schinnbehn/controller/Controller.java
+++ src/main/java/se/lu/sysa11/schinnbehn/controller/Controller.java
@@ -5,9 +5,12 @@ import se.lu.sysa11.schinnbehn.gui.Window;
 
 /**
  * Base class for all controllers?
+ * 
  * @author Matteus
- * @param <GuiType> the GUI/View class
- * @param <RegisterType> the Register/Model class
+ * @param <GuiType>
+ *            the GUI/View class
+ * @param <RegisterType>
+ *            the Register/Model class
  */
 public class Controller<GuiType extends Gui<?>, RegisterType> {
 	/** Application Window */
@@ -17,12 +20,15 @@ public class Controller<GuiType extends Gui<?>, RegisterType> {
 	/** Register/Model */
 	protected RegisterType register;
 
-
 	/**
 	 * Constructor that takes the application window
-	 * @param window application window
-	 * @param gui GUI/view object for the controller
-	 * @param register Register/model object for the controller
+	 * 
+	 * @param window
+	 *            application window
+	 * @param gui
+	 *            GUI/view object for the controller
+	 * @param register
+	 *            Register/model object for the controller
 	 */
 	protected Controller(Window window, GuiType gui, RegisterType register) {
 		this.window = window;
--- src/main/java/se/lu/sysa11/schinnbehn/controller/CustomerController.java
+++ src/main/java/se/lu/sysa11/schinnbehn/controller/CustomerController.java
@@ -11,9 +11,13 @@ import se.lu.sysa11.schinnbehn.gui.Window;
 public class CustomerController extends Controller<CustomerGui, CustomerReg> {
 	/**
 	 * Create a custom controller with the window
-	 * @param window application window
-	 * @param customerGui the customer view/GUIö
-	 * @param customerReg the customer model/register
+	 * 
+	 * @param window
+	 *            application window
+	 * @param customerGui
+	 *            the customer view/GUIö
+	 * @param customerReg
+	 *            the customer model/register
 	 */
 	protected CustomerController(Window window, CustomerGui customerGui, CustomerReg customerReg) {
 		super(window, customerGui, customerReg);
@@ -22,13 +26,19 @@ public class CustomerController extends Controller<CustomerGui, CustomerReg> {
 
 	/**
 	 * Create a new customer
-	 * @param name customer/company name
-	 * @param telephone company telephone number
-	 * @param address company address
-	 * @param email company email address
-	 * @param billingAddress where to send the bills
-	 * @return true if the customer was added successfully. False if one or more of the
-	 *         parameters are wrong or empty
+	 * 
+	 * @param name
+	 *            customer/company name
+	 * @param telephone
+	 *            company telephone number
+	 * @param address
+	 *            company address
+	 * @param email
+	 *            company email address
+	 * @param billingAddress
+	 *            where to send the bills
+	 * @return true if the customer was added successfully. False if one or more
+	 *         of the parameters are wrong or empty
 	 */
 	public boolean addCustomer(String name, String telephone, String address, String email, String billingAddress) {
 		boolean successfull = false;
@@ -44,16 +54,24 @@ public class CustomerController extends Controller<CustomerGui, CustomerReg> {
 
 	/**
 	 * Update an existing customer
-	 * @param customerNbr the customer number
-	 * @param name customer/company name
-	 * @param telephone company telephone number
-	 * @param address company address
-	 * @param email company email address
-	 * @param billingAddress where to send the bills
-	 * @return true if the customer was changed successfully. False if one or more of the
-	 *         parameters are wrong or empty.
+	 * 
+	 * @param customerNbr
+	 *            the customer number
+	 * @param name
+	 *            customer/company name
+	 * @param telephone
+	 *            company telephone number
+	 * @param address
+	 *            company address
+	 * @param email
+	 *            company email address
+	 * @param billingAddress
+	 *            where to send the bills
+	 * @return true if the customer was changed successfully. False if one or
+	 *         more of the parameters are wrong or empty.
 	 */
-	public boolean updateCustomer(String customerNbr, String name, String telephone, String address, String email, String billingAddress) {
+	public boolean updateCustomer(String customerNbr, String name, String telephone, String address, String email,
+			String billingAddress) {
 		boolean successfull = false;
 
 		// TODO check if valid
@@ -67,9 +85,11 @@ public class CustomerController extends Controller<CustomerGui, CustomerReg> {
 
 	/**
 	 * Find an existing customer
-	 * @param searchString what to search for (currently only the customer number)
-	 * @return list of all found customer (currently the list can only contain one or
-	 *         zero)
+	 * 
+	 * @param searchString
+	 *            what to search for (currently only the customer number)
+	 * @return list of all found customer (currently the list can only contain
+	 *         one or zero)
 	 */
 	public Customer findCustomer(String searchString) {
 		// TODO implement find Customer

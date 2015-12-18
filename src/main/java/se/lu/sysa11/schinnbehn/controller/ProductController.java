329ba17ae3039a4145815c019689705f5339cd10
jesperfransson <jesperfransson@10.4.3.112>
2015-12-18 15:19:24 +0100

    completed updatedMethod in controller

--- src/main/java/se/lu/sysa11/schinnbehn/controller/ProductController.java
+++ src/main/java/se/lu/sysa11/schinnbehn/controller/ProductController.java
@@ -12,9 +12,12 @@ import se.lu.sysa11.schinnbehn.gui.Window;
  */
 public class ProductController extends Controller<ProductGui, ProductReg> {
 	/**
-	 * @param window the application window
-	 * @param productGui GUI/view of the product controller
-	 * @param productReg Register/model of the product controller
+	 * @param window
+	 *            the application window
+	 * @param productGui
+	 *            GUI/view of the product controller
+	 * @param productReg
+	 *            Register/model of the product controller
 	 */
 	protected ProductController(Window window, ProductGui productGui, ProductReg productReg) {
 		super(window, productGui, productReg);
@@ -24,15 +27,23 @@ public class ProductController extends Controller<ProductGui, ProductReg> {
 
 	/**
 	 * Add a new product
-	 * @param productNbr number/serial of the product
-	 * @param name product name
-	 * @param price current price of the product
-	 * @param ingredients ingredients of the product
-	 * @param weight weight of the product
-	 * @param cost how much it costs to buy or produce the product
+	 *
+	 * @param productNbr
+	 *            number/serial of the product
+	 * @param name
+	 *            product name
+	 * @param price
+	 *            current price of the product
+	 * @param ingredients
+	 *            ingredients of the product
+	 * @param weight
+	 *            weight of the product
+	 * @param cost
+	 *            how much it costs to buy or produce the product
 	 * @return true if successfully added product
 	 */
-	public boolean addProduct(String productNbr, String name, double price, String ingredients, double weight, double cost) {
+	public boolean addProduct(String productNbr, String name, double price, String ingredients, double weight,
+			double cost) {
 		boolean successful = false;
 
 		// TODO check if valid
@@ -40,6 +51,14 @@ public class ProductController extends Controller<ProductGui, ProductReg> {
 		// TODO if invalid -> Send notification
 
 		// TODO Add product
+		Product product = new Product();
+		product.setProductNbr(productNbr);
+		product.setName(name);
+		product.setPrice(price);
+		product.setIngredients(ingredients);
+		product.setWeight(weight);
+		product.setCost(cost);
+		register.add(product);
 
 		// TODO send notification
 
@@ -48,15 +67,23 @@ public class ProductController extends Controller<ProductGui, ProductReg> {
 
 	/**
 	 * Update an existing product
-	 * @param productNbr number/serial of the product
-	 * @param name product name
-	 * @param price current price of the product
-	 * @param ingredients ingredients of the product
-	 * @param weight weight of the product
-	 * @param cost how much it costs to buy or produce the product
+	 *
+	 * @param productNbr
+	 *            number/serial of the product
+	 * @param name
+	 *            product name
+	 * @param price
+	 *            current price of the product
+	 * @param ingredients
+	 *            ingredients of the product
+	 * @param weight
+	 *            weight of the product
+	 * @param cost
+	 *            how much it costs to buy or produce the product
 	 * @return true if successfully added product
 	 */
-	public boolean updateProduct(String productNbr, String name, double price, String ingredients, double weight, double cost) {
+	public boolean updateProduct(String productNbr, String name, double price, String ingredients, double weight,
+			double cost) {
 		boolean successful = false;
 
 		// TODO check if valid
@@ -64,6 +91,15 @@ public class ProductController extends Controller<ProductGui, ProductReg> {
 		// TODO if invalid -> Send notification
 
 		// TODO Update product
+		Product product = register.findProduct(productNbr);
+		if (product != null) {
+			product.setProductNbr(productNbr);
+			product.setName(name);
+			product.setPrice(price);
+			product.setIngredients(ingredients);
+			product.setWeight(weight);
+			product.setCost(cost);
+		}
 
 		// TODO send notification
 
@@ -72,9 +108,11 @@ public class ProductController extends Controller<ProductGui, ProductReg> {
 
 	/**
 	 * Find existing products
-	 * @param searchString what to search for (currently only the product number)
-	 * @return list of all found products (currently the list only contains either one or
-	 *         zero)
+	 *
+	 * @param searchString
+	 *            what to search for (currently only the product number)
+	 * @return list of all found products (currently the list only contains
+	 *         either one or zero)
 	 */
 	public ArrayList<Product> findProduct(String searchString) {
 		// TODO implement find custom

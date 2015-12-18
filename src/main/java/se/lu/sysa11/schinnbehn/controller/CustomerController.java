d131b1badd8ede86932ceeb88bf186a8b6d6b963
senth <senth.wallace@gmail.com>
2015-12-18 11:44:49 +0100

    Added stub methods in CustomerController and added constructors to the other controllers.
    Controller now has a window, register, and gui variables.

--- src/main/java/se/lu/sysa11/schinnbehn/controller/CustomerController.java
+++ src/main/java/se/lu/sysa11/schinnbehn/controller/CustomerController.java
@@ -1,8 +1,78 @@
 package se.lu.sysa11.schinnbehn.controller;
 
+import se.lu.sysa11.schinnbehn.Customer;
+import se.lu.sysa11.schinnbehn.CustomerReg;
+import se.lu.sysa11.schinnbehn.gui.CustomerGui;
+import se.lu.sysa11.schinnbehn.gui.Window;
+
 /**
  * Connects the customer model and view
  */
-public class CustomerController {
+public class CustomerController extends Controller<CustomerGui, CustomerReg> {
+	/**
+	 * Create a custom controller with the window
+	 * @param window application window
+	 * @param customerGui the customer view/GUIö
+	 * @param customerReg the customer model/register
+	 */
+	protected CustomerController(Window window, CustomerGui customerGui, CustomerReg customerReg) {
+		super(window, customerGui, customerReg);
+	}
 
+	/**
+	 * Create a new customer
+	 * @param name customer/company name
+	 * @param telephone company telephone number
+	 * @param address company address
+	 * @param email company email address
+	 * @param billingAddress where to send the bills
+	 * @return true if the customer was added successfully. False if one or more of the
+	 *         parameters are wrong or empty
+	 */
+	public boolean addCustomer(String name, String telephone, String address, String email, String billingAddress) {
+		boolean successfull = false;
+
+		// TODO check if valid
+
+		// TODO Add customer
+
+		// TODO send notification
+
+		return successfull;
+	}
+
+	/**
+	 * Update an existing customer
+	 * @param customerNbr the customer number
+	 * @param name customer/company name
+	 * @param telephone company telephone number
+	 * @param address company address
+	 * @param email company email address
+	 * @param billingAddress where to send the bills
+	 * @return true if the customer was changed successfully. False if one or more of the
+	 *         parameters are wrong or empty.
+	 */
+	public boolean updateCustomer(String customerNbr, String name, String telephone, String address, String email, String billingAddress) {
+		boolean successfull = false;
+
+		// TODO check if valid
+
+		// TODO update customer
+
+		// TODO send notification
+
+		return successfull;
+	}
+
+	/**
+	 * Find an existing customer
+	 * @param searchString what to search for (currently only the customer number)
+	 * @return list of all found customer (currently the list can only contain one or
+	 *         zero)
+	 */
+	public Customer findCustomer(String searchString) {
+		// TODO implement find Customer
+
+		return null;
+	}
 }
--- src/main/java/se/lu/sysa11/schinnbehn/controller/OrderController.java
+++ src/main/java/se/lu/sysa11/schinnbehn/controller/OrderController.java
@@ -1,8 +1,22 @@
 package se.lu.sysa11.schinnbehn.controller;
 
+import se.lu.sysa11.schinnbehn.OrderReg;
+import se.lu.sysa11.schinnbehn.gui.OrderGui;
+import se.lu.sysa11.schinnbehn.gui.Window;
+
 /**
  * The order controller
  */
-public class OrderController {
+public class OrderController extends Controller<OrderGui, OrderReg> {
+	/**
+	 * @param window the application window
+	 * @param gui GUI/view of the product controller
+	 * @param register Register/model of the product controller
+	 */
+	protected OrderController(Window window, OrderGui gui, OrderReg register) {
+		super(window, gui, register);
+		// TODO Auto-generated constructor stub
+	}
 
+	// TODO
 }

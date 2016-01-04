5f99d073889b51e8a68dfd2751eb1e1c3ac6f9a5
paulssonkalle <paulsson.kalle@gmail.com>
2016-01-04 09:37:16 +0100

    Made some changes in the searchCustomer function.

--- src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
+++ src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
@@ -28,11 +28,9 @@ public class CustomerGui extends Gui<CustomerController> {
 	private JTextField textField_Phone;
 	private JTextField textField_Adress;
 	private JTextField textField_Email;
-	private JTextField textField_PostalNbr;
 	private JTextField textField_FindCustomer;
 	private JTextField textField_ShowCustomerNbr;
 	private JTable table_Orders;
-	private JTextField textField_City;
 
 	/**
 	 * @wbp.parser.entryPoint
@@ -55,39 +53,26 @@ public class CustomerGui extends Gui<CustomerController> {
 		lblPhoneNumber.setBounds(12, 86, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblPhoneNumber);
 
-		JLabel lblStreetAdress = new JLabel("Gatuadress:");
-		lblStreetAdress.setBounds(12, 115, LABEL_WIDTH, LABEL_HEIGHT);
-		panel.add(lblStreetAdress);
-
-		JLabel lblPostalNbr = new JLabel("Postnummer:");
-		lblPostalNbr.setBounds(12, 144, LABEL_WIDTH, LABEL_HEIGHT);
-		panel.add(lblPostalNbr);
-
-		JLabel lblCity = new JLabel("Stad:");
-		lblCity.setBounds(12, 173, LABEL_WIDTH, LABEL_HEIGHT);
-		panel.add(lblCity);
+		JLabel lblAdress = new JLabel("Adress:");
+		lblAdress.setBounds(12, 115, LABEL_WIDTH, LABEL_HEIGHT);
+		panel.add(lblAdress);
 
 		JLabel lblEmail = new JLabel("Email:");
-		lblEmail.setBounds(12, 202, LABEL_WIDTH, LABEL_HEIGHT);
+		lblEmail.setBounds(12, 145, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblEmail);
 
 		JLabel lblFoundCustomer = new JLabel("Kundnr:");
-		lblFoundCustomer.setBounds(12, 231, LABEL_WIDTH, LABEL_HEIGHT);
+		lblFoundCustomer.setBounds(12, 174, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblFoundCustomer);
 
 		JLabel lblFindCustomer = new JLabel("Hitta kund");
 		lblFindCustomer.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblFindCustomer.setBounds(12, 328, LABEL_WIDTH, LABEL_HEIGHT);
+		lblFindCustomer.setBounds(12, 271, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblFindCustomer);
 
 		JLabel lblKundnr = new JLabel("Kundnr:");
-		lblKundnr.setBounds(12, 361, LABEL_WIDTH, LABEL_HEIGHT);
+		lblKundnr.setBounds(12, 304, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblKundnr);
-
-		textField_City = new JTextField();
-		textField_City.setBounds(158, 173, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
-		panel.add(textField_City);
-		textField_City.setColumns(10);
 
 		textField_Name = new JTextField();
 		textField_Name.setBounds(158, 57, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
@@ -105,23 +90,18 @@ public class CustomerGui extends Gui<CustomerController> {
 		textField_Adress.setColumns(10);
 
 		textField_Email = new JTextField();
-		textField_Email.setBounds(158, 202, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
+		textField_Email.setBounds(158, 145, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
 		panel.add(textField_Email);
 		textField_Email.setColumns(10);
 
-		textField_PostalNbr = new JTextField();
-		textField_PostalNbr.setBounds(158, 144, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
-		panel.add(textField_PostalNbr);
-		textField_PostalNbr.setColumns(10);
-
 		textField_FindCustomer = new JTextField();
-		textField_FindCustomer.setBounds(158, 361, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
+		textField_FindCustomer.setBounds(158, 304, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
 		panel.add(textField_FindCustomer);
 		textField_FindCustomer.setColumns(10);
 
 		textField_ShowCustomerNbr = new JTextField();
 		textField_ShowCustomerNbr.setEditable(false);
-		textField_ShowCustomerNbr.setBounds(158, 231, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
+		textField_ShowCustomerNbr.setBounds(158, 174, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
 		panel.add(textField_ShowCustomerNbr);
 		textField_ShowCustomerNbr.setColumns(10);
 
@@ -131,14 +111,13 @@ public class CustomerGui extends Gui<CustomerController> {
 			public void actionPerformed(ActionEvent e) {
 				String name = textField_Name.getText();
 				String phoneNbr = textField_Phone.getText();
-				String adress = textField_Adress.getText() + " " + textField_PostalNbr.getText() + " " + textField_City.getText();
+				String adress = textField_Adress.getText();
 				String email = textField_Email.getText();
-
 
 				controller.addCustomer(name, phoneNbr, adress, email);
 			}
 		});
-		btnAddCustomer.setBounds(12, 273, BUTTON_WIDTH, BUTTON_HEIGHT);
+		btnAddCustomer.setBounds(12, 216, BUTTON_WIDTH, BUTTON_HEIGHT);
 		panel.add(btnAddCustomer);
 
 		JButton btnSearchCustomer = new JButton("S\u00F6k");
@@ -149,11 +128,12 @@ public class CustomerGui extends Gui<CustomerController> {
 				Customer tmpCustomer = controller.findCustomer(searchString);
 				textField_Name.setText(tmpCustomer.getName());
 				textField_Phone.setText(tmpCustomer.getTelephoneNbr());
-				textField_ShowCustomerNbr.setText(tmpCustomer.getCustomerNbr());
+				textField_Adress.setText(tmpCustomer.getAddress());
 				textField_Email.setText(tmpCustomer.getEmail());
+				textField_ShowCustomerNbr.setText(tmpCustomer.getCustomerNbr());
 			}
 		});
-		btnSearchCustomer.setBounds(309, 361, BUTTON_WIDTH, BUTTON_HEIGHT);
+		btnSearchCustomer.setBounds(309, 304, BUTTON_WIDTH, BUTTON_HEIGHT);
 		panel.add(btnSearchCustomer);
 
 		JButton btnUpdateCustomer = new JButton("Uppdatera kund");
@@ -163,13 +143,13 @@ public class CustomerGui extends Gui<CustomerController> {
 				String customerNbr = textField_ShowCustomerNbr.getText();
 				String name = textField_Name.getText();
 				String telephone = textField_Phone.getText();
-				String address = textField_Adress.getText() + " " + textField_PostalNbr.getText();
+				String address = textField_Adress.getText();
 				String email = textField_Email.getText();
 
 				controller.updateCustomer(customerNbr, name, telephone, address, email);
 			}
 		});
-		btnUpdateCustomer.setBounds(158, 273, BUTTON_WIDTH, BUTTON_HEIGHT);
+		btnUpdateCustomer.setBounds(158, 216, BUTTON_WIDTH, BUTTON_HEIGHT);
 		panel.add(btnUpdateCustomer);
 
 		JScrollPane scrollPane = new JScrollPane();

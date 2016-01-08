c65eb70e4057f72925e9f113f496b5ca0dfccab9
jesperfransson <jesperfransson@10.2.241.125>
2016-01-08 10:16:05 +0100

    DID ADD A NEW TABLE IN CUSTOMER_GUI

--- src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
+++ src/main/java/se/lu/sysa11/schinnbehn/gui/CustomerGui.java
@@ -28,9 +28,13 @@ public class CustomerGui extends Gui<CustomerController> {
 		super(window);
 	}
 
-	private static final int COLUMN_DATE = 0;
-	private static final int COLUMN_ID = 1;
-	private static final int COLUMN_SUM = 2;
+	private static final int ORDER_COLUMN_DATE = 0;
+	private static final int ORDER_COLUMN_ID = 1;
+	private static final int ORDER_COLUMN_SUM = 2;
+
+	private static final int CUSTOMER_COLUMN_ID = 0;
+	private static final int CUSTOMER_COLUMN_NAME = 1;
+	private static final int CUSTOMER_COLUMN_ADRESS = 2;
 
 	/**
 	 * Can't have panel in base class as we're not able to access WindowBuilder
@@ -44,8 +48,10 @@ public class CustomerGui extends Gui<CustomerController> {
 	private JTextField textField_FindCustomer;
 	private JTextField textField_ShowCustomerNbr;
 	private JTable table_Orders;
-	private DefaultTableModel table_Model;
+	private DefaultTableModel tableModel_Order;
+	private DefaultTableModel tableModel_Customer;
 	private JTextField textField_OrdersTotal;
+	private JTable table_Customer;
 
 	/**
 	 * @wbp.parser.entryPoint
@@ -82,11 +88,11 @@ public class CustomerGui extends Gui<CustomerController> {
 
 		JLabel lblFindCustomer = new JLabel("Hitta kund");
 		lblFindCustomer.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblFindCustomer.setBounds(12, 271, LABEL_WIDTH, LABEL_HEIGHT);
+		lblFindCustomer.setBounds(25, 341, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblFindCustomer);
 
 		JLabel lblKundnr = new JLabel("Kundnr:");
-		lblKundnr.setBounds(12, 304, LABEL_WIDTH, LABEL_HEIGHT);
+		lblKundnr.setBounds(25, 378, LABEL_WIDTH, LABEL_HEIGHT);
 		panel.add(lblKundnr);
 
 		textField_Name = new JTextField();
@@ -110,7 +116,7 @@ public class CustomerGui extends Gui<CustomerController> {
 		textField_Email.setColumns(10);
 
 		textField_FindCustomer = new JTextField();
-		textField_FindCustomer.setBounds(158, 304, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
+		textField_FindCustomer.setBounds(133, 378, TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
 		panel.add(textField_FindCustomer);
 		textField_FindCustomer.setColumns(10);
 
@@ -146,8 +152,8 @@ public class CustomerGui extends Gui<CustomerController> {
 				Customer tmpCustomer = controller.findCustomer(searchString);
 				double sum = 0;
 
-				while (table_Model.getRowCount() > 0) {
-					table_Model.removeRow(0);
+				while (tableModel_Order.getRowCount() > 0) {
+					tableModel_Order.removeRow(0);
 				}
 
 				if (tmpCustomer != null) {
@@ -160,7 +166,7 @@ public class CustomerGui extends Gui<CustomerController> {
 					for (Order tmpOrder : tmpCustomer.getOrders().values()) {
 						int orderNbr = Integer.parseInt(tmpOrder.getOrderNbr());
 						Object[] row = { tmpOrder.getOrderDate(), orderNbr, tmpOrder.getTotalPrice() };
-						table_Model.addRow(row);
+						tableModel_Order.addRow(row);
 						sum += tmpOrder.getTotalPrice();
 
 					}
@@ -175,7 +181,7 @@ public class CustomerGui extends Gui<CustomerController> {
 				textField_OrdersTotal.setText(Double.toString(sum));
 			}
 		});
-		btnSearchCustomer.setBounds(304, 304, BUTTON_WIDTH, BUTTON_HEIGHT);
+		btnSearchCustomer.setBounds(304, 379, BUTTON_WIDTH, BUTTON_HEIGHT);
 		panel.add(btnSearchCustomer);
 
 		JButton btnUpdateCustomer = new JButton("Uppdatera kund");
@@ -195,11 +201,11 @@ public class CustomerGui extends Gui<CustomerController> {
 		panel.add(btnUpdateCustomer);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(459, 24, 452, 362);
+		scrollPane.setBounds(459, 24, 452, 277);
 		panel.add(scrollPane);
 
 		String column_names[] = { "Datum", "Ordernummer", "Summa (exkl. moms)" };
-		table_Model = new DefaultTableModel(new Object[][] {}, column_names) {
+		tableModel_Order = new DefaultTableModel(new Object[][] {}, column_names) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
@@ -210,11 +216,11 @@ public class CustomerGui extends Gui<CustomerController> {
 			@Override
 			public Class<?> getColumnClass(int columnIndex) {
 				switch (columnIndex) {
-				case COLUMN_DATE:
+				case ORDER_COLUMN_DATE:
 					return String.class;
-				case COLUMN_ID:
+				case ORDER_COLUMN_ID:
 					return Integer.class;
-				case COLUMN_SUM:
+				case ORDER_COLUMN_SUM:
 					return Double.class;
 				default:
 					return String.class;
@@ -225,14 +231,14 @@ public class CustomerGui extends Gui<CustomerController> {
 
 		table_Orders = new JTable();
 		table_Orders.setAutoCreateRowSorter(true);
-		table_Orders.setModel(table_Model);
+		table_Orders.setModel(tableModel_Order);
 		table_Orders.getColumnModel().getColumn(0).setResizable(false);
 		scrollPane.setViewportView(table_Orders);
 		table_Orders.addMouseListener(new TableClickListener() {
 			@Override
 			public void onDoubleClick(JTable table, int row) {
 				try {
-					Object value = table_Model.getValueAt(row, COLUMN_ID);
+					Object value = tableModel_Order.getValueAt(row, ORDER_COLUMN_ID);
 					if (value instanceof Integer) {
 						String orderNumber = String.valueOf(value);
 						controller.gotoOrder(orderNumber);
@@ -260,15 +266,55 @@ public class CustomerGui extends Gui<CustomerController> {
 		textField_OrdersTotal = new JTextField();
 		textField_OrdersTotal.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_OrdersTotal.setEditable(false);
-		textField_OrdersTotal.setBounds(765, 403, 146, 26);
+		textField_OrdersTotal.setBounds(763, 313, 146, 26);
 		panel.add(textField_OrdersTotal);
 		textField_OrdersTotal.setColumns(10);
 
 		JLabel lblTotaltexklMoms = new JLabel("Totalt (exkl. moms):");
-		lblTotaltexklMoms.setBounds(625, 406, 138, 20);
+		lblTotaltexklMoms.setBounds(624, 313, 138, 20);
 		panel.add(lblTotaltexklMoms);
 
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(25, 411, 411, 254);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(0, 0, 411, 254);
+		panel_1.add(scrollPane_1);
+
+		table_Customer = new JTable();
+		scrollPane_1.setViewportView(table_Customer);
+
 		setInitialized(true);
+
+		String column_namesCustomer[] = { "Kundnummer", "Namn", "Adress" };
+		tableModel_Customer = new DefaultTableModel(new Object[][] {}, column_namesCustomer) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+
+			@Override
+			public Class<?> getColumnClass(int columnIndex) {
+				switch (columnIndex) {
+				case ORDER_COLUMN_DATE:
+					return String.class;
+				case ORDER_COLUMN_ID:
+					return Integer.class;
+				case ORDER_COLUMN_SUM:
+					return Double.class;
+				default:
+					return String.class;
+				}
+			}
+
+		};
+
+		table_Customer.setAutoCreateRowSorter(true);
+		table_Customer.setModel(tableModel_Customer);
 
 	}
 
